209a11fa4f35264828e901771d5bdb39b479f924
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2021-06-14 13:52:17 +0000

    git-init-githubrepo: add 0.2.0 bottle.

--- Formula/git-init-githubrepo.rb
+++ Formula/git-init-githubrepo.rb
@@ -5,6 +5,12 @@ class GitInitGithubrepo < Formula
   sha256 "19b7fff21d9a7e26ba661fe6eb5ed92a0451694f13144ace4a76baf8fbd020f0"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/vigo/homebrew-git-init-githubrepo/releases/download/git-init-githubrepo-0.2.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "e93666f044c83267a35f097264da46ac4a69175e5a0e70cfc2b6e3e4cac76b9b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "fc657235d34a214ba9a1674f7f294b8dd5d1c4fb6d07d509613abe786e4b8529"
+  end
+
   depends_on "go" => :build
 
   def install
